73e97d49de5d53ed18937263d0f9a631bb5da02f
Anderson-10 <andersonhurtado182@hotmail.com>
2022-12-10 00:28:30 -0500

    Rate and Areas information

--- asssignment-4.py
+++ asssignment-4.py
@@ -21,3 +21,18 @@ class Area:
 
 employees = {}
 areas = {}
+
+
+human_resources_manager = Area(1,'Human Resources','manager',42.0,63.0)
+human_resources_assistant = Area(2,'Human Resources','assistant',18.0,27.0)
+finance_assistant = Area(3,'Finance','assistant',24.0,36.0)
+finance_Analyst = Area(4,'Finance','Analyst',38.0,57.0)
+IT_support = Area(5,'Finance','Analyst',28.0,42.0)
+IT_devepoler = Area(5,'Finance','Analyst',40.0,60.0)
+
+areas[human_resources_manager.id_area] = human_resources_manager
+areas[human_resources_assistant.id_area] = human_resources_assistant
+areas[finance_assistant.id_area] = finance_assistant
+areas[finance_Analyst.id_area] = finance_Analyst
+areas[IT_support.id_area] = IT_support
+areas[IT_devepoler.id_area] = IT_devepoler
