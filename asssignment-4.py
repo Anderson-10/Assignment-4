34dc0738a99027de299f62c98a370a6ce785f78f
Anderson-10 <andersonhurtado182@hotmail.com>
2022-12-10 14:12:25 -0500

    Add funtion isInt and solve problems in menu

--- asssignment-4.py
+++ asssignment-4.py
@@ -27,8 +27,8 @@ human_resources_manager = Area(1,'Human Resources','manager',42.0,63.0)
 human_resources_assistant = Area(2,'Human Resources','assistant',18.0,27.0)
 finance_assistant = Area(3,'Finance','assistant',24.0,36.0)
 finance_Analyst = Area(4,'Finance','Analyst',38.0,57.0)
-IT_support = Area(5,'Finance','Analyst',28.0,42.0)
-IT_devepoler = Area(5,'Finance','Analyst',40.0,60.0)
+IT_support = Area(5,'IT','IT Support',28.0,42.0)
+IT_devepoler = Area(6,'IT','Developer',40.0,60.0)
 
 areas[human_resources_manager.id_area] = human_resources_manager
 areas[human_resources_assistant.id_area] = human_resources_assistant
@@ -37,7 +37,25 @@ areas[finance_Analyst.id_area] = finance_Analyst
 areas[IT_support.id_area] = IT_support
 areas[IT_devepoler.id_area] = IT_devepoler
 
-
+def isInt (message, isValidateArea, isValidateEmployee):
+    while True:
+        num = input(message)
+         
+        try:
+            numInt = int(num)
+            if isValidateArea == False and isValidateEmployee == False:
+                return numInt
+            if(isValidateArea == True and numInt >= 1 and numInt <=len(areas)):
+                return numInt
+            if(isValidateEmployee == True and numInt >= 1 and numInt <=len(employees)):
+                return numInt
+            if(isValidateArea == True):
+                print("Please select a range between 1 - "+str(len(areas)))
+            if(isValidateEmployee == True):
+                print("Please select a range between 1 - "+str(len(employees)))
+             
+        except ValueError:
+            print("Please add a correct value ")
 
 def newEmployee():
     id = int(len(employees) + 1) 
@@ -46,7 +64,7 @@ def newEmployee():
     print("%-30s %-15s %-15s %-15s %-15s" %('id_area','area','position','value_hour','value_overtime'))
     for i in areas:
              print("%-30s %-15s %-15s %-15s %-15s" %(str(areas[i].id_area), str(areas[i].area), str(areas[i].position),  str(areas[i].value_hour), str(areas[i].value_overtime)))
-    position = input("What is the position of the employee (select the id):")
+    position = isInt("What is the position of the employee (select the id):",True,False)
     employees[id] = Employee(id, name, position )
 
 def isIntOption (message):
